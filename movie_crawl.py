18f56ab48351b479e5b890150a0a8314959048e1
ogorman89 <ogorman89@gmail.com>
2023-06-22 15:00:48 -0700

    added csv dependency, now writes crawl results to a csv file in the current directory

--- movie_crawl.py
+++ movie_crawl.py
@@ -1,37 +1,43 @@
 import requests
+import csv
 from bs4 import BeautifulSoup
 
-base_url = "https://www.boxofficemojo.com/"
+# start a csv file and provide headers
+with open('box_office.csv', 'w', newline='') as file:
+    writer = csv.writer(file)
+    field = ["rank", "title", "revenue"]
 
-# this is the url where the list starts
-url = "https://www.boxofficemojo.com/chart/top_lifetime_gross/?area=XWW"
+    base_url = "https://www.boxofficemojo.com/"
 
-while True:
-    # get request the html page, parse the page using BeautifulSoup
-    page = requests.get(url)
-    soup = BeautifulSoup(page.content, "html.parser")
+    # this is the url where the list starts
+    url = "https://www.boxofficemojo.com/chart/top_lifetime_gross/?area=XWW"
 
-    # find the table element that holds the data we want
-    results = soup.find(id='table')
+    while True:
+        # get request the html page, parse the page using BeautifulSoup
+        page = requests.get(url)
+        soup = BeautifulSoup(page.content, "html.parser")
 
-    # find all table rows within the table element
-    rows = results.find_all("tr")
+        # find the table element that holds the data we want
+        results = soup.find(id='table')
 
-    # example row from src
-    # <tr>
-    #   <td class="a-text-right mojo-header-column mojo-truncate mojo-field-type-rank">1</td>
-    #   <td class="a-text-left mojo-field-type-title"><a class="a-link-normal" href="/title/tt0499549/?ref_=bo_cso_table_1">Avatar</a></td>
-    #   <td class="a-text-right mojo-field-type-money">$2,923,706,026</td><td class="a-text-left mojo-field-type-year"><a class="a-link-normal" href="/year/world/2009/?ref_=bo_cso_table_1">2009</a></td>
-    # </tr>
+        # find all table rows within the table element
+        rows = results.find_all("tr")
 
-    # loop through all table rows to parse out the rank, title and revenue based on element and class
-    # slices first row headers
-    for row in rows[1:]:
-        rank = row.find("td", class_="mojo-field-type-rank").text
-        title = row.find("td", class_="mojo-field-type-title").text
-        revenue = row.find("td", class_="mojo-field-type-money").text
-        print(rank, title, revenue)
-    try:
-        url = base_url + soup.find("li", class_='a-last').find("a")['href']
-    except TypeError:
-        break
+        # example row from src
+        # <tr>
+        #   <td class="a-text-right mojo-header-column mojo-truncate mojo-field-type-rank">1</td>
+        #   <td class="a-text-left mojo-field-type-title"><a class="a-link-normal" href="/title/tt0499549/?ref_=bo_cso_table_1">Avatar</a></td>
+        #   <td class="a-text-right mojo-field-type-money">$2,923,706,026</td><td class="a-text-left mojo-field-type-year"><a class="a-link-normal" href="/year/world/2009/?ref_=bo_cso_table_1">2009</a></td>
+        # </tr>
+
+        # loop through all table rows to parse out the rank, title and revenue based on element and class
+        # slices first row headers
+        for row in rows[1:]:
+            rank = row.find("td", class_="mojo-field-type-rank").text
+            title = row.find("td", class_="mojo-field-type-title").text
+            revenue = row.find("td", class_="mojo-field-type-money").text
+            writer.writerow([rank, title, revenue])
+        try:
+            url = base_url + soup.find("li", class_='a-last').find("a")['href']
+        except TypeError:
+            break
