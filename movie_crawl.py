c0087fb6221ba8a9fa0f62ea556ecce7734fd5e5
ogorman89 <ogorman89@gmail.com>
2023-06-22 15:19:25 -0700

    removed an example file, parsed revenue column so it only contains numbers

--- movie_crawl.py
+++ movie_crawl.py
@@ -33,9 +33,11 @@ with open('box_office.csv', 'w', newline='') as file:
         # loop through all table rows to parse out the rank, title and revenue based on element and class
         # slices first row headers
         for row in rows[1:]:
-            rank = row.find("td", class_="mojo-field-type-rank").text
-            title = row.find("td", class_="mojo-field-type-title").text
-            revenue = row.find("td", class_="mojo-field-type-money").text
+            rank = row.find("td", class_="mojo-field-type-rank").text.strip()
+            title = row.find("td", class_="mojo-field-type-title").text.strip()
+            revenue = row.find("td", class_="mojo-field-type-money").text.strip()
+            for i in ['"','$',',']:
+                revenue = revenue.replace(i,"")
             writer.writerow([rank, title, revenue])
         try:
             url = base_url + soup.find("li", class_='a-last').find("a")['href']
